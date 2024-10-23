14e246e6ab2b897f4abf75d117cfbf174875844b
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-10-23 19:04:10 -0300

    Change cv download

--- src/components/DownloadBtn.jsx
+++ src/components/DownloadBtn.jsx
@@ -1,9 +1,9 @@
-import CV_file from "../assets/Mariano_Garmendia_CV_Dev.docx";
+import Cv_file from "..assets/Mariano_G_CV_.pdf";
 
 const DownloadButton = () => {
   return (
     <a
-      href={CV_file}
+      href={Cv_file}
       download="Mariano_Garmendia_CV_Dev.docx"
       className="rounded-full bg-white px-4 py-2 text-sm font-bold text-[#1f1f1f] w-fit hover:scale-105 transition-all duration-200 ease-in-out "
     >
